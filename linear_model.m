9be7de901408cd353344e14291313437e199ff5e
sivak049 <sivak049@umn.edu>
2024-04-21 02:15:27 -0500

    Different solver for linear; works perfectly now

--- linear_model.m
+++ linear_model.m
@@ -5,12 +5,12 @@ syms subs_lost_price adv_lost subs_lost_adv Subs_lost_per_advert_decrease mu0 mu
 C0_substitution_constants = [Init_number_subs, subs_lost_price, Init_price_subs, Init_price_advert, subs_lost_adv, adv_lost];
 C0_substitution_values = [80000, 5000/.1, 1.50, 250, 1000/50, 50/100];
 
-C1_substitution_constants = [Init_number_adverts, adv_lost, subs_lost_adv, Init_price_advert];
-C1_substitution_values = [350,  50/100, 1000/50, 250];
+C1_substitution_constants = [Init_number_adverts, adv_lost, Init_price_advert];
+C1_substitution_values = [350,  50/100, 250];
 
 C0 = Init_number_subs - (x-Init_price_subs)*(subs_lost_price) - (y-Init_price_advert) * subs_lost_adv * adv_lost;
 C0_subs = subs(C0, C0_substitution_constants, C0_substitution_values);
-disp(C0_subs);
+%disp(C0_subs);
 C1 = Init_number_adverts - (y-Init_price_advert) * adv_lost;
 C1_subs = subs(C1, C1_substitution_constants, C1_substitution_values);
 
@@ -24,7 +24,7 @@ R = C0 * x + C1 * y;
 
 R_subs = C0_subs * x + C1_subs * y;
 
-disp(R_subs);
+%disp(R_subs);
 % Calculate the gradient
 grad = gradient(R_subs, [x, y]);
 
@@ -33,11 +33,12 @@ assume(x,'real');
 assumeAlso(0 <= x);
 assume(y, 'real');
 assumeAlso(0 <= y);
-sol = vpasolve(grad == [0; 0], [x, y], [1.5, 500]);
-
-% disp(sol);
-disp(double(subs(subs(R_subs, x, 1.53), y, 359)));
-disp(double(subs(subs(R_subs, x, 1.53), y, 600)));
+%sol = vpasolve(grad == [0; 0], [x, y], [1.5, 500]);
+sol = fsolve(@(vars) double(subs(grad, [x, y], vars)), [1.5, 250]);
+%disp(double(subs(subs(grad, x, 1.52902), y, 459.7067)));
+%disp(sol);
+%disp(double(subs(subs(R_subs, x, 1.53), y, 359)));
+%disp(double(subs(subs(R_subs, x, 1.53), y, 600)));
 %[x_opt, y_opt] = solve([dR_dx == 0, dR_dy == 0], [x, y]);
 %value = 255;
 %x_opt_val = subs(x_opt, v, value);
@@ -62,7 +63,7 @@ disp(double(subs(subs(R_subs, x, 1.53), y, 600)));
 %%% Sensitivity Analysis %%%
 
 substitution_constants = [Init_number_subs, adv_lost, Init_price_subs, Init_number_adverts, subs_lost_adv, subs_lost_price, Init_price_advert, x, y];
-substitution_values = [80000, 50/100, 1.50, 350,  1000/50, 5000/.1, 250, 1.539039039039039039039039039039, 359.60960960960960960960960960961];
+substitution_values = [80000, 50/100, 1.50, 350,  1000/50, 5000/.1, 250, 1.5290292912452677, 459.7067436151746];
 
 dR_dInit_price_subs = subs(diff(R, Init_price_subs), substitution_constants, substitution_values);
 dR_dInit_number_subs = subs(diff(R, Init_number_subs), substitution_constants, substitution_values);
@@ -83,8 +84,8 @@ sensitivity_mu1 = (1000/50 / R_val) * dR_dmu1;
 sensitivity_lam = (5000/.1 / R_val) * dR_dlam;
 
 %disp(double(sensitivity_Init_price_subs));
-disp(['optimal x = ' num2str(1.539039039039039039039039039039)]);
-disp(['optimal y = ' num2str(359.60960960960960960960960960961)]);
+disp(['optimal x = ' num2str(sol(1))]);
+disp(['optimal y = ' num2str(sol(2))]);
 disp(['maximized objective function = ' num2str(double(R_val))]);
 disp('Sensitivity analysis:');
 disp(['Sensitivity of Init_price_subs: ' num2str(double(sensitivity_Init_price_subs))]);
@@ -94,8 +95,7 @@ disp(['Sensitivity of init_number_adverts: ' num2str(double(sensitivity_Init_num
 disp(['Sensitivity of advs_count: ' num2str(double(sensitivity_mu0))]);
 disp(['Sensitivity of subs_lost_adv: ' num2str(double(sensitivity_mu1))]);
 disp(['Sensitivity of subs_lost_price: ' num2str(double(sensitivity_lam))]);
-disp(R)
-% 
+ 
 % %{
 % Results:
 % optimal x = 1.8765
